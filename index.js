17dc83381fa98e3de3283d4b262c5e0ef6543c39
Qize <louisliu1206@yahoo.com>
2020-06-10 04:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase 
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can\'t hear you!";
